c6d9aa3bf1be21f71ba6d6e9ae0c21885548595c
Bartek Pacia <barpac02@gmail.com>
2018-11-09 14:44:50 +0100

    Added full functionality

--- bot.js
+++ bot.js
@@ -5,9 +5,9 @@ const TOKEN = '639410307:AAGs7mw314gU7axsg7ey1LGFv-Kdu7QoAjI';
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 //example url for testing
-const repoUrl = 'https://api.github.com/repos/JBossOutreach/lead-management-android';
+const baseUrl = 'https://api.github.com/repos/JBossOutreach/';
 
-bot.onText(/\/getstars (.+)/, (msg, match) => {
+bot.onText(/\/getinfo (.+)/, (msg, match) => {
     // 'msg' is the received Message from Telegram
     // 'match' is the result of executing the regexp above on the text content
     // of the message
@@ -15,8 +15,12 @@ bot.onText(/\/getstars (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const enteredText = match[1];
 
+    var finalUrl = baseUrl + enteredText;
+    console.log(finalUrl);
+    
+
     var options = {
-        url: repoUrl,
+        url: finalUrl,
         json: true,
         headers: {
             'User-Agent': 'request'
@@ -28,10 +32,22 @@ bot.onText(/\/getstars (.+)/, (msg, match) => {
             return console.log(error);
         }
 
-        //print watchers so it can be easily debugged
-        console.log(body.watchers);
+        var name = body.name;
+        var language = body.language;
+        var license = body.license.name;
+        var stars = body.watchers;
+        var forks = body.forks;
+        var githubLink = body.html_url;
+        
+        var output = "Repository " + name +
+                    "\nMain language: " + language +
+                    "\nLicense: " + license +
+                    "\nStars: " + stars + 
+                    "\nForks: " + forks +
+                    "\nGithub link: " + githubLink;
 
-        bot.sendMessage(chatId, "Response code " + response.statusCode);
+        bot.sendMessage(chatId, output);
+        console.log("Status code: " + response.statusCode);
     }
 
     request(options, callback); 
