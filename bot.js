7f6c78b982b52330b8a926a7f9de7ffbb19b32b5
Bartek Pacia <barpac02@gmail.com>
2018-11-10 11:58:48 +0100

    Final improvements before submitting the task.
    -Added README
    -Added /help handling
    -Added output message formatting

--- bot.js
+++ bot.js
@@ -23,9 +23,10 @@ bot.onText(/\/getinfo (.+)/, (msg, match) => {
   // of the message
   const chatId = msg.chat.id;
   const enteredText = match[1];
-  console.log(`Entered text: ${enteredText}`);
   const linksArray = [];
-  let finalUrl = 'NULL';
+  let finalUrl;
+
+  console.log(`---New query--- \nValue: ${enteredText}`);
 
   // Make a final request to get data from specific repo and send it to user
   function finalRequest() {
@@ -34,11 +35,15 @@ bot.onText(/\/getinfo (.+)/, (msg, match) => {
         console.error(error);
         return;
       }
+      // Print response status
+      console.log(`Status code: ${response.statusCode} ${response.statusMessage}`);
+
       const name = body.name;
       const language = body.language;
       const stars = body.watchers;
       const forks = body.forks;
       const githubLink = body.html_url;
+      // Some repositories don't have license
       let license;
       if (body.license != null) {
         license = body.license.name;
@@ -46,12 +51,17 @@ bot.onText(/\/getinfo (.+)/, (msg, match) => {
         license = 'No license';
       }
 
-      const output = `Repository ${name}\nMain language: ${language}\nLicense: ${license}\nStars: ${stars}\nForks: ${forks}\nGithub link: ${githubLink}`;
+      // Markdown-styled text that will be sent
+      const output = `
+Closest match for your query is
+*${name}*
+Main language: ${language}
+License: ${license}
+Stars: ${stars}
+Forks: ${forks}
+Github: ${githubLink}`;
 
-      bot.sendMessage(chatId, output);
-
-      // Print response status
-      console.log(`Status code: ${response.statusCode} ${response.statusMessage}`);
+      bot.sendMessage(chatId, output, { parse_mode: 'Markdown' });
     });
   }
 
@@ -67,12 +77,25 @@ bot.onText(/\/getinfo (.+)/, (msg, match) => {
       const link = gotJSON[i].name;
       linksArray.push(link);
     }
-    console.log(linksArray);
 
     const matches = similarity.findBestMatch(enteredText, linksArray);
     const bestMatch = matches.bestMatch.target;
     finalUrl = baseUrl + bestMatch;
-    console.log(`Final url: ${finalUrl}`);
+    console.log(`Best match: ${bestMatch}, url ${finalUrl} will be queried`);
     finalRequest();
   });
+});
+
+// Provide help feature
+bot.onText(/\/help/, (msg) => {
+  // 'msg' is the received Message from Telegram
+
+  const chatId = msg.chat.id;
+
+  const helpMessage = `
+/getinfo repository
+
+*For example:* /getinfo leadandro *or* /getinfo lead-management-android`;
+
+  bot.sendMessage(chatId, helpMessage, { parse_mode: 'Markdown' });
 });
