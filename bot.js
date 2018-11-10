b3027f9c8c7a1128487e70863a3a114eee4c502d
Bartek Pacia <barpac02@gmail.com>
2018-11-10 21:52:00 +0100

    Changed 'request' to 'request-promise-native'
    -Huge improvements in code readability (using async/await)
    -Small changes to ESLint config

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 // Load .env file that has TOKEN
 require('dotenv').config();
 
-const request = require('request');
+const request = require('request-promise-native');
 const TelegramBot = require('node-telegram-bot-api');
 const similarity = require('string-similarity');
 
@@ -17,83 +17,88 @@ const requestOptions = {
   },
 };
 
-bot.onText(/\/getinfo (.+)/, (msg, match) => {
+console.log('Bot is running...');
+
+bot.onText(/\/getinfo (.+)/, async (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
-  // of the message
+  // of the message (so in this case, basically everything following /getinfo)
   const chatId = msg.chat.id;
   const enteredText = match[1];
-  const linksArray = [];
+  console.log(`---New query--- \nQuery: ${enteredText}`);
+
+  const url = await processUrl(enteredText);
+  const message = await processMessage(url);
+  bot.sendMessage(chatId, message, { parse_mode: 'Markdown' });
+});
+
+// Makes a request to get an array of URLs to which we can compare text entered by user
+// Returns URL of the most probable repository
+async function processUrl(text) {
   let finalUrl;
-
-  console.log(`---New query--- \nValue: ${enteredText}`);
-
-  // Make a final request to get data from specific repo and send it to user
-  function finalRequest() {
-    request(finalUrl, requestOptions, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      // Print response status
-      console.log(`Status code: ${response.statusCode} ${response.statusMessage}`);
-
-      const name = body.name;
-      const language = body.language;
-      const stars = body.watchers;
-      const forks = body.forks;
-      const githubLink = body.html_url;
-      // Some repositories don't have license
-      let license;
-      if (body.license != null) {
-        license = body.license.name;
-      } else {
-        license = 'No license';
-      }
-
-      // Markdown-styled text that will be sent
-      const output = `
-Closest match for your query is
-*${name}*
-Main language: ${language}
-License: ${license}
-Stars: ${stars}
-Forks: ${forks}
-Github: ${githubLink}`;
-
-      bot.sendMessage(chatId, output, { parse_mode: 'Markdown' });
-    });
-  }
-
-  // Make first request to have an array of URLs to query
-  request(reposUrl, requestOptions, (error, response, body) => {
+  await request(reposUrl, requestOptions, (error, response, body) => {
     if (error) {
       console.error(error);
-      return;
     }
 
+    const linksArray = [];
     const gotJSON = body;
     for (let i = 0; i < gotJSON.length; i++) {
       const link = gotJSON[i].name;
       linksArray.push(link);
     }
 
-    const matches = similarity.findBestMatch(enteredText, linksArray);
+    const matches = similarity.findBestMatch(text, linksArray);
     const bestMatch = matches.bestMatch.target;
     finalUrl = baseUrl + bestMatch;
     console.log(`Best match: ${bestMatch}, url ${finalUrl} will be queried`);
-    finalRequest();
   });
-});
+
+  return finalUrl;
+}
+
+// Makes a request to get data of the particular repository
+async function processMessage(url) {
+  console.log(`URL: ${url}`);
+  let message;
+  await request(url, requestOptions, (error, response, body) => {
+    if (error) {
+      console.error(error);
+    }
+    // Print response status
+    console.log(`Status code: ${response.statusCode} ${response.statusMessage}`);
+
+    const name = body.name;
+    const language = body.language;
+    const stars = body.watchers;
+    const forks = body.forks;
+    const githubLink = body.html_url;
+    // Some repositories don't have license
+    let license;
+    if (body.license != null) {
+      license = body.license.name;
+    } else {
+      license = 'No license';
+    }
+
+    // Markdown-styled text that will be sent
+    message = `Closest match for your query is
+*${name}*
+Main language: ${language}
+License: ${license}
+Stars: ${stars}
+Forks: ${forks}
+Github: ${githubLink}`;
+  });
+
+  return message;
+}
 
 // Provide help feature
 bot.onText(/\/help/, (msg) => {
-  // 'msg' is the received Message from Telegram
-
   const chatId = msg.chat.id;
 
-  const helpMessage = `
-/getinfo repository
+  const helpMessage = `/getinfo repository
 
 *For example:* /getinfo leadandro *or* /getinfo lead-management-android`;
 
